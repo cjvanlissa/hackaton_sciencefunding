fdf76036d8261838436e3283eed70e4a67b9c100
cjvanlissa <c.j.vanlissa@uu.nl>
2022-07-13 12:28:52 +0200

    import script

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -6,6 +6,8 @@
 library(worcs)
 library(openxlsx)
 library(haven)
+
+# # Parse text, no longer necessary
 # dat <- readLines("../data/DJA_forDJA.csv")
 # headers <- dat[1]
 # headers <- strsplit(headers, split = ",")[[1]]
@@ -18,8 +20,16 @@ library(haven)
 # names(testfile) <- headers
 # write.csv(testfile, "dja.csv", row.names = FALSE)
 
+# Read STATA
 dat <- haven::read_dta("../data/DJA_forDJA.dta")
+# Rename
+# namz <- openxlsx::read.xlsx("../data/Variabelen_DJA_shortlabels.xlsx")
+# write.csv(namz, "../data/Variabelen_DJA_shortlabels.csv", row.names = F)
 namz <- read.csv("../data/Variabelen_DJA_shortlabels.csv")
+
+names(dat)[match(namz$Variabele.naam, names(dat))] <- namz$short
+
+
 factors <- sapply(dat, function(x){
   !is.null(names(attr(x, "label")))
 })
@@ -29,4 +39,16 @@ dat[factors] <- lapply(dat[factors], function(x){
 })
 type <- sapply(dat, function(x){class(x)[1]})
 
-openxlsx::write.xlsx(dat, "dja.xlsx")
+secretfactor <- sapply(dat, function(x){length(table(x))}) < 23 & type == "character"
+tmp <- dat[, secretfactor]
+dat$language <- factor(dat$language)
+dat$work_life[dat$work_life %in% c("Ja, in grote mate", "Yes, to a big degree")] <- 0
+dat$work_life[dat$work_life %in% c( "Yes, to a small degree", "Ja, in kleine mate")] <- 1
+dat$work_life[dat$work_life %in% c("Nee", "No")] <- 3
+dat$work_life[dat$work_life %in% c("Wil / kan ik niet zeggen", "I don't want to say","")] <- NA
+dat$work_life <- as.integer(dat$work_life)
+type <- sapply(dat, function(x){class(x)[1]})
+dat <- dat[, !type == "character"]
+
+write.csv(dat, "../data/dat_cj.csv", row.names = F)
+dat <- read.csv("../data/dat_cj.csv", stringsAsFactors = TRUE)
